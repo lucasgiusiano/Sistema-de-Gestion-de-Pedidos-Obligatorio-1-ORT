ae645038a6e7bd10559d642b5a5955e495f7928d
vmeloni90 <118079748+vmeloni90@users.noreply.github.com>
2024-03-23 17:16:02 -0300

    Estructura inicial del Negocio

---
+++ SistemaGestionNegocio/Cliente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionNegocio
+{
+    public class Cliente
+    {
+    }
+}
