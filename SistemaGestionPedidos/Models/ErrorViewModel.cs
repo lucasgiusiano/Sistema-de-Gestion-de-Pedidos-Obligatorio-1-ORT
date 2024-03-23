9300cd46e3cfc642ae38c1ea8dba5ecb002393d2
vmeloni90 <118079748+vmeloni90@users.noreply.github.com>
2024-03-23 17:01:35 -0300

    Agregar archivos de proyecto.

---
+++ SistemaGestionPedidos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaGestionPedidos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
