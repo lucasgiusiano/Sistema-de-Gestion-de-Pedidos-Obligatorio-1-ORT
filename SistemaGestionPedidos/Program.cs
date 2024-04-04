e692d056c4455be7c0bce671d4b8d63ff977f4d4
vmeloni90 <118079748+vmeloni90@users.noreply.github.com>
2024-04-04 14:10:59 -0300

    Update Program
    Se agregan los builders que faltaban de las interfaces , casos de uso.

--- SistemaGestionPedidos/Program.cs
+++ SistemaGestionPedidos/Program.cs
@@ -3,6 +3,15 @@ using SistemaGestionDatos;
 using SistemaGestionAplicacion.CasosUso;
 using SistemaGestionNegocio.InterfacesRepositorio;
 using SistemaGestionDatos.Repositorios;
+using SistemaGestionNegocio.Dominio;
+using SistemaGestionAplicacion.CasosUso.CUArticulo;
+using SistemaGestionAplicacion.InterfacesCU.ICUGenericas;
+using SistemaGestionAplicacion.InterfacesCU.ICUArticulo;
+using SistemaGestionAplicacion.CasosUso.CUCliente;
+using SistemaGestionAplicacion.InterfacesCU.ICUCliente;
+using SistemaGestionAplicacion.CasosUso.CUPedido;
+using SistemaGestionAplicacion.InterfacesCU.ICUPedido;
+using SistemaGestionAplicacion.CasosUso.CUUsuario;
 
 
 
@@ -19,6 +28,19 @@ namespace SistemaGestionPedidos
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped <ICUAlta<Articulo>, CUAltaArticulo> ();
+            builder.Services.AddScoped<ICUListadoOrdenadoArticulos, CUListadoOrdenadoArticulos>();
+            builder.Services.AddScoped<ICUBuscarClientesPorMonto, CUBuscarClientesPorMonto>();
+            builder.Services.AddScoped<ICUBuscarClientesPorNombre, CUBuscarClientesPorNombre>();
+            builder.Services.AddScoped<ICUAlta<Pedido>, CUAltaPedido>();
+            builder.Services.AddScoped<ICUAnularPedido, CUAnularPedido>();
+            builder.Services.AddScoped<ICUListadoPedidosAnuladosXFecha, CUListadoPedidosAnuladosXFecha>();
+            builder.Services.AddScoped<ICUAlta<Usuario>, CUAltaUsuario>();
+            builder.Services.AddScoped<ICUBaja, CUBajaUsuario>();
+            builder.Services.AddScoped<ICUBuscar<Usuario>, CUBuscarUsuario>();
+            builder.Services.AddScoped<ICUListado<Usuario>, CUListadoUsuario>();
+            builder.Services.AddScoped<ICUModificar<Usuario>, CUModificarUsuario>();
 
             builder.Services.AddScoped<IRepositorioArticulo, RepositorioArticulo>();
             builder.Services.AddScoped<IRepositorioCliente, RepositorioCliente>();
