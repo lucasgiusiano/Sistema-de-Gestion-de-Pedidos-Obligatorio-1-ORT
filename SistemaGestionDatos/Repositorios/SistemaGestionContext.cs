757553a48c4bdbf31d2196a8eef9028246459fc5
vmeloni90 <118079748+vmeloni90@users.noreply.github.com>
2024-04-03 22:55:47 -0300

    Clase Context, Clase Program
    Cree la clase Context, instale los nugets, Agregue en la clase program los repositorios (falta terminar).

---
+++ SistemaGestionDatos/Repositorios/SistemaGestionContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaGestionNegocio.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionDatos.Repositorios
+{
+    public class SistemaGestionContext: DbContext
+    {
+        public DbSet<Articulo> Articulos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLOCALDB; Initial Catalog = SistemaGestionPedidos; Integrated Security=SSPI;");
+            }
+        }
+    }
+}
