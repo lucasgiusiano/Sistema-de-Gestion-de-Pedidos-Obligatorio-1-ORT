9421ca5c4be0701fe915dec2a6a2e19d512c6488
Lucas Giusiano <luucass200026@gmail.com>
2024-04-06 05:33:19 -0300

    Inicio de modelado de ViewModels

--- SistemaGestionPedidos/Models/UsuarioViewModel.cs
+++ SistemaGestionPedidos/Models/UsuarioViewModel.cs
@@ -2,5 +2,9 @@
 {
     public class UsuarioViewModel
     {
+        public string Email { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Contrasenia { get; set; }
     }
 }
