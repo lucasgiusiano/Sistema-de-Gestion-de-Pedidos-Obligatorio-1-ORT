c1edd5d2d3c35023e5c8ef6fdebce5fd197f6793
Lucas Giusiano <luucass200026@gmail.com>
2024-05-04 20:24:57 -0300

    Controller de Articulo en API

---
+++ SistemaGestionAPI/Controllers/ArticuloController.cs
@@ -0,0 +1,36 @@
+﻿using DTOs.DTOs_Articulo;
+using Microsoft.AspNetCore.Mvc;
+using SistemaGestionAplicacion.InterfacesCU.ICUArticulo;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SistemaGestionAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ArticuloController : ControllerBase
+	{
+		private readonly ICUListadoOrdenadoArticulos CUListado;
+
+		public ArticuloController(ICUListadoOrdenadoArticulos cUListado)
+		{
+			CUListado = cUListado;
+		}
+
+
+		// GET: api/<ArticuloController>
+		[HttpGet]
+		public ActionResult<IEnumerable<DTOAltaArticulo>> GetListadoOrdenadoDeArticulos()
+		{
+			try
+			{
+				return Ok(CUListado.ListadoOrdenado());
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(StatusCodes.Status500InternalServerError, $"Error al cargar los articulos: {ex.Message}");
+			}
+			
+		}
+	}
+}
