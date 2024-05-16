fad883a80236d0193c197a3687fb0c45c0293727
Lucas Giusiano <luucass200026@gmail.com>
2024-05-16 01:46:42 -0300

    Update HomeController.cs

--- SistemaGestionPedidos/Controllers/HomeController.cs
+++ SistemaGestionPedidos/Controllers/HomeController.cs
@@ -25,11 +25,6 @@ namespace SistemaGestionPedidos.Controllers
             }
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
